03b4f4a90b981c4208d40343d6c19778afa9457e
haroonrana <haroonrana224@gmail.com>
2021-10-05 19:27:06 +0500

    Sign in added and sign up hide

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:registrations]
+as :user do
+  get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+  get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  put 'users' => 'devise/registrations#update', :as => 'user_registration'
+end 
+
+  get 'home/about'
   root "home#index"
 end
+
+  
