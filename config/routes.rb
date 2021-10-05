248629f880cf691bebb5b8cfe69d267cdd2d9512
haroonrana <haroonrana224@gmail.com>
2021-10-06 01:22:26 +0500

    update routes and delete sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations]
-as :user do
-  get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-  get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-  put 'users' => 'devise/registrations#update', :as => 'user_registration'
-end 
+  devise_for :users, skip: :registrations
+
 
   get 'home/about'
   root "home#index"
